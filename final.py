42742bd22ce7e40ceaff3bab5a4b0cb7232d2c07
NI3singh <itsnits333@gmail.com>
2025-02-25 13:14:54 +0530

    changed default choice from None to male

--- final.py
+++ final.py
@@ -109,7 +109,7 @@ def predict_gender(face_image):
 @app.post("/predict-gender")
 async def predict_gender_endpoint(
     file: UploadFile = File(...),
-    selected_gender: Gender = None
+    selected_gender: Gender = Gender.MALE
 ):
     try:
         # Read image file
